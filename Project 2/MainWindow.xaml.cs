91d25c76c7d957f934277d44e6a823de4105c400
Sergio <santosguy101@gmail.com>
2018-02-08 19:25:58 -0500

    Initial steps

--- Project 2/MainWindow.xaml.cs
+++ Project 2/MainWindow.xaml.cs
@@ -20,9 +20,42 @@ namespace Project_2
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Model model;
         public MainWindow()
         {
             InitializeComponent();
+            model = new Model();
+            this.DataContext = model;
+        }
+
+        private void AddButt_Click(object sender, RoutedEventArgs e)
+        {
+            model.Op = Model.CurrentOp.ADD;
+        }
+
+        private void MinButt_Click(object sender, RoutedEventArgs e)
+        {
+            model.Op = Model.CurrentOp.SUB;
+        }
+
+        private void MultButt_Click(object sender, RoutedEventArgs e)
+        {
+            model.Op = Model.CurrentOp.MULT;
+        }
+
+        private void DivButt_Click(object sender, RoutedEventArgs e)
+        {
+            model.Op = Model.CurrentOp.DIV;
+        }
+
+        private void EqualsButt_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ExitButt_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
         }
     }
 }
---
+++ Project 2/Model.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_2
+{
+    class Model : INotifyPropertyChanged
+
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+#region op
+        public enum CurrentOp { ADD, SUB, MULT, DIV, NONE };
+
+        private CurrentOp _op;
+        public CurrentOp Op
+        {
+            get { return _op; }
+            set
+            {
+                _op = value;
+                OnPropertyChanged("OpBox");
+            }
+        }
+        #endregion
+
+        private float _firstNum;
+        public float FirstNum
+        {
+            get { return _firstNum; }
+            set
+            {
+                _firstNum = value;
+            }
+        }
+
+        private float _secondNum;
+        public float SecondNum
+        {
+            get { return _secondNum; }
+            set
+            {
+                _secondNum = value;
+            }
+        }
+
+        private float _resultNum;
+        public float ResultNum
+        {
+            get { return _resultNum; }
+            set
+            {
+                _resultNum = value;
+            }
+        }
+
+        private void OnPropertyChanged(string propName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propName));
+            }
+        }
+
+        public Model()
+        {
+            Op = CurrentOp.NONE;
+        }
+    }
+}
